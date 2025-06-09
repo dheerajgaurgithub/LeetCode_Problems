841ebbfcbae3ec8c27527a295c2f56cf8baefafe
DheerajGaur.Official <dheeraj.ajaygaur05@gmail.com>
2025-06-09 12:12:31 +0530

    Time: 3 ms (41.10%) | Memory: 42 MB (31.87%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,22 +1,21 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        Map<Character, Character> map = new HashMap<>();
+        Stack<Character> stack=new Stack<>();
+        Map<Character,Character> map=new HashMap<>();
         map.put(')', '(');
         map.put('}', '{');
         map.put(']', '[');
-        
-        for (char c : s.toCharArray()) {
-            if (map.containsKey(c)) {
+        for(char c:s.toCharArray()){
+            if(map.containsKey(c)){
                 char top = stack.isEmpty() ? '#' : stack.pop();
                 if (top != map.get(c)) {
                     return false;
-                }
-            } else {
+                } 
+            }
+            else {
                 stack.push(c);
             }
         }
-        
         return stack.isEmpty();
     }
 }
