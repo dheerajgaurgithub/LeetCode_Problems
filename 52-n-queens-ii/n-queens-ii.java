5f93ba659b80177efa2fad1faca2bbcdd11804a8
DheerajGaur.Official <dheeraj.ajaygaur05@gmail.com>
2025-06-23 09:29:54 +0530

    Time: 2 ms (62.17%) | Memory: 40.4 MB (75.03%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.java
@@ -0,0 +1,47 @@
+class Solution {
+    int count=0;
+    public int totalNQueens(int n) {
+        char[][]board=new char[n][n];
+        for(char[] row:board){
+            Arrays.fill(row,'.');
+        }
+        countSolutions(board,0);
+        return count;
+    }
+     public void countSolutions(char[][] board, int row) {
+        int n = board.length;
+
+        if (row == n) {
+            count++;
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = 'Q'; // Place queen
+                countSolutions(board, row + 1); // Recurse
+                board[row][col] = '.'; // Backtrack
+            }
+        }
+    }
+    public boolean isSafe(char[][] board, int row, int col) {
+        int n = board.length;
+
+        // Check column
+        for (int i = 0; i < row; i++)
+            if (board[i][col] == 'Q')
+                return false;
+
+        // Check top-left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
+            if (board[i][j] == 'Q')
+                return false;
+
+        // Check top-right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++)
+            if (board[i][j] == 'Q')
+                return false;
+
+        return true;
+    }
+}
