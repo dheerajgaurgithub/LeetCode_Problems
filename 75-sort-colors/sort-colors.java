689a3f33f1100c167bd3f8a114761c569d75287d
DheerajGaur.Official <dheeraj.ajaygaur05@gmail.com>
2025-06-26 22:22:44 +0530

    Time: 13 ms (2.67%) | Memory: 42.7 MB (9.92%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,18 +1,22 @@
 class Solution {
     public void sortColors(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[j] < nums[minIndex]) {
-                    minIndex = j;
-                }
-            }
-            // Swap nums[i] and nums[minIndex]
-            int temp = nums[i];
-            nums[i] = nums[minIndex];
-            nums[minIndex] = temp;
+        int count0 = 0, count1 = 0, count2 = 0;
+
+        // Count occurrences
+        for (int num : nums) {
+            if (num == 0) count0++;
+            else if (num == 1) count1++;
+            else count2++;
         }
 
+        // Overwrite array with counted values
+        int index = 0;
+
+        while (count0-- > 0) nums[index++] = 0;
+        while (count1-- > 0) nums[index++] = 1;
+        while (count2-- > 0) nums[index++] = 2;
+
+        // Print result
         for (int i = 0; i < nums.length; i++) {
             System.out.print(nums[i] + " ");
         }
