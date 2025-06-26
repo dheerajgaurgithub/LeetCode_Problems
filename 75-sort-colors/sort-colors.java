dd1210f7f17b4918d27d54d9565667454923eaac
DheerajGaur.Official <dheeraj.ajaygaur05@gmail.com>
2025-06-26 22:23:24 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (68.00%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,24 +1,24 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int count0 = 0, count1 = 0, count2 = 0;
-
-        // Count occurrences
-        for (int num : nums) {
-            if (num == 0) count0++;
-            else if (num == 1) count1++;
-            else count2++;
+    int zero = 0,one = 0,two = 0;
+    for(int i = 0; i < nums.length;i++){
+        if(nums[i] == 0){
+         zero++;
+        }else if(nums[i] == 1){
+            one++;
+        }else{
+            two++;
         }
-
-        // Overwrite array with counted values
-        int index = 0;
-
-        while (count0-- > 0) nums[index++] = 0;
-        while (count1-- > 0) nums[index++] = 1;
-        while (count2-- > 0) nums[index++] = 2;
-
-        // Print result
-        for (int i = 0; i < nums.length; i++) {
-            System.out.print(nums[i] + " ");
-        }
+    }
+     int i = 0;
+     for(int j = 0;j<zero;j++){
+        nums[i++] = 0;
+     }
+     for(int j = 0; j < one;j++){
+        nums[i++] = 1;
+     }
+     for(int j = 0; j < two;j++){
+        nums[i++] = 2;
+     }
     }
 }
