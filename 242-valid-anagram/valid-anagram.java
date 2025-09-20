36665ae68f8b0be71e3bbd921762455983d544ff
DheerajGaur.Official <dheeraj.ajaygaur05@gmail.com>
2025-09-21 00:08:13 +0530

    Time: 4 ms (76.39%) | Memory: 44.6 MB (43.71%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        char[] x=s.toCharArray();
+        char[] y=t.toCharArray();
+        Arrays.sort(x);
+        Arrays.sort(y);
+        return Arrays.equals(x,y);
+    }
+}
